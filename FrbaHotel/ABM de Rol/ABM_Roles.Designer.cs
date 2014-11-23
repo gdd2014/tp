3a32cc667b10c9659738fbb56699f553d3a0c2c4
Gonza Guglielmo <gonzaloguglielmo@gmail.com>
2014-11-23 19:36:36 -0300

    Primeros pasos de ABM

---
+++ FrbaHotel/ABM de Rol/ABM_Roles.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace FrbaHotel.ABM_de_Rol
+{
+    partial class ABM_Roles {
+
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.titulo = new System.Windows.Forms.Label();
+            this.tablaDeRoles = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.tablaDeRoles)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // titulo
+            // 
+            this.titulo.AutoSize = true;
+            this.titulo.Location = new System.Drawing.Point(32, 21);
+            this.titulo.Name = "titulo";
+            this.titulo.Size = new System.Drawing.Size(184, 13);
+            this.titulo.TabIndex = 0;
+            this.titulo.Text = "Altas bajas y modificaciones de Roles";
+            // 
+            // tablaDeRoles
+            // 
+            this.tablaDeRoles.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tablaDeRoles.Location = new System.Drawing.Point(35, 64);
+            this.tablaDeRoles.Name = "tablaDeRoles";
+            this.tablaDeRoles.Size = new System.Drawing.Size(444, 150);
+            this.tablaDeRoles.TabIndex = 1;
+            // 
+            // ABM_Roles
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(550, 298);
+            this.Controls.Add(this.tablaDeRoles);
+            this.Controls.Add(this.titulo);
+            this.Name = "ABM_Roles";
+            this.Text = "ABM de Roles";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.tablaDeRoles)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titulo;
+        private System.Windows.Forms.DataGridView tablaDeRoles;
+
+
+    }
+}
---
+++ FrbaHotel/ABM de Rol/ABM_Roles.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.SqlClient;
+using System.Data;
+using System.Data.Sql;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FrbaHotel.ABM_de_Rol
+{
+    public partial class ABM_Roles : Form
+    {
+        public ABM_Roles()
+        {
+            InitializeComponent();
+        }
+
+        System.Data.SqlClient.SqlConnection con;
+
+        private void Form1_Load(object sender, EventArgs e) {
+            con = new System.Data.SqlClient.SqlConnection();
+            con.ConnectionString = "Data Source=localhost\\SQLSERVER2008;Initial Catalog=GD2C2014;Persist Security Info=True;User ID=gd;Password=gd2014";
+
+            SqlCommand cmd = new SqlCommand();
+
+            String query = "SELECT Hotel_Dom_Calle AS Dirección, Hotel_Estrellas AS Estrellas FROM G_N.Hoteles";
+
+            con.Open();
+
+            DataSet dataSet = new DataSet();
+            SqlDataAdapter dataAdapter = new SqlDataAdapter(query, con);
+            dataAdapter.SelectCommand.CommandTimeout = 600;
+            dataAdapter.Fill(dataSet);
+            tablaDeRoles.DataSource = dataSet.Tables[0];
+
+            con.Close();
+            
+        }
+    }
+}
