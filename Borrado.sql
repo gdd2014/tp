d98c2836e4701b87029f47ea53cb02eda36c90f9
Natt <natalia.duarte.87@gmail.com>
2014-12-10 15:47:23 -0300

    script borrado

---
+++ Borrado.sql
@@ -0,0 +1,23 @@
+
+DROP PROCEDURE G_N.Resetear_Login_fallido
+DROP PROCEDURE G_N.Registrar_Login_fallido
+
+DROP TABLE G_N.Hoteles_Cerrados
+DROP TABLE G_N.Roles_Funcionalidades
+DROP TABLE G_N.Funcionalidades
+DROP TABLE G_N.Usuarios_Roles
+DROP TABLE G_N.Roles
+DROP TABLE G_N.Usuarios_Hoteles
+DROP TABLE G_N.Usuarios
+DROP TABLE G_N.Factura_Items
+DROP TABLE G_N.Facturas
+DROP TABLE G_N.Estadias
+DROP TABLE G_N.Reservas
+DROP TABLE G_N.Consumibles
+DROP TABLE G_N.Clientes
+DROP TABLE G_N.Documento_Tipos
+DROP TABLE G_N.Habitaciones
+DROP TABLE G_N.Habitacion_Tipos
+DROP TABLE G_N.Hoteles_Regimenes
+DROP TABLE G_N.Regimenes
+DROP TABLE G_N.Hoteles
